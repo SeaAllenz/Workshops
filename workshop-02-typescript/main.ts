48eec9195892feb61d436292e319c88cfaf86d28
Sean Allen <seaallen0519@gmail.com>
2024-09-15 11:11:28 -0500

    modified, tc

--- workshop-02-typescript/main.ts
+++ workshop-02-typescript/main.ts
@@ -1,1 +1,13 @@
-// Fill in during tutorial!
+// Fill in during tutorial! 
+
+let userName: "Sean"
+let favoriteFood: string[] = ["pizza", "tacos"]
+
+let user: {userName: string,  age: number, favoriteFood: string} = {
+    userName: "sees",
+    age:30,
+    favoriteFood:"pizza"
+
+}
+
+console.log(user.favoriteFood) //Logs users fav food --> pizza
